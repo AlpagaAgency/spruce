84fc69db7e7f6ed0759c355d8f842192e1db9288
Frédéric Faltin <ffaltin@alpaga.agency>
2018-10-27 10:31:59 +0200

    Add remove default wp action rsd, xrpc, etc

--- src/Kernel/Site.php
+++ src/Kernel/Site.php
@@ -114,7 +114,9 @@ class Site extends TimberSite {
 	protected function remove_actions()
 	{
 		remove_action('wp_head', 'wp_generator');
-
+        remove_action( 'wp_head', 'wlwmanifest_link');
+		remove_action( 'wp_head', 'wp_shortlink_wp_head');
+		remove_action ('wp_head', 'rsd_link');
 		return $this;
 	}
 
