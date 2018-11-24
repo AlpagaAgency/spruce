8448ebc6238de4d76842bc3e2af5f80b3d47c05f
Frédéric Faltin <ffaltin@alpaga.agency>
2018-11-24 17:12:47 +0100

    Add acf helpers on post

--- src/Model/Post.php
+++ src/Model/Post.php
@@ -9,6 +9,9 @@ use Spruce\Model\Image;
 
 class Post extends TimberPost {
 
+	protected $acfFields = null;
+    protected $factories = null;
+
 	public function getLanguage() {
 		if( function_exists('pll_get_post_language') ):
 			return pll_get_post_language($this->id);
@@ -26,5 +29,38 @@ class Post extends TimberPost {
 		$attachId = Image::createFeaturedImage($path, $postId);
 		return new Image($attachId);
 	}
+
+	public function addFactories($factories) 
+    {
+        $this->factories = $factories;
+        return $this;
+    }
+
+    protected function getAcfFields() 
+    {
+        if (is_null($this->acfFields))
+        {
+            $this->acfFields = get_field_objects($this->ID);
+        }
+        return $this->acfFields;
+	}
+	
+	public function getTemplateType()
+    {
+        return str_replace([".php", "tpl-"], "", $this->_wp_page_template);
+	}
+	
+	public function getPicture($pictureId)
+    {
+        return new Image($pictureId);
+    }
+
+    public function getACFValue($node)
+    {
+        $acf = $this->getAcfFields();
+        if (!isset($acf[$node]))
+            return [];
+        return $acf[$node]["value"];
+    }
 
 }
