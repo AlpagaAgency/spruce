55d3f27959a31be1ba4cc9d25f0acbfd8e7201b9
Frédéric Faltin <ffaltin@alpaga.agency>
2021-12-24 16:20:02 +0100

    Remove autodiscover oembed

--- src/Kernel/Site.php
+++ src/Kernel/Site.php
@@ -158,6 +158,45 @@ class Site extends TimberSite {
 		add_action( 'init', array( $this, 'disable_emojis' ));
 		add_action('admin_head', array( $this, 'admin_styles' ));
 		add_action( 'login_enqueue_scripts', [$this, 'my_login_style'] );
+		//
+		add_action( 'wp_enqueue_scripts', function (){
+			wp_dequeue_style( 'wp-block-library' );
+			wp_dequeue_style( 'wp-block-library-theme' );
+			wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+		} , 100 );
+		
+		add_action( 'init', function() {
+			// Remove the REST API endpoint.
+			remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+		   
+			// Turn off oEmbed auto discovery.
+			add_filter( 'embed_oembed_discover', '__return_false' );
+		   
+			// Don't filter oEmbed results.
+			remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+		   
+			// Remove oEmbed discovery links.
+			remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+		   
+			// Remove oEmbed-specific JavaScript from the front-end and back-end.
+			remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+			add_filter( 'tiny_mce_plugins', function ($plugins) {
+				return array_diff($plugins, array('wpembed'));
+			});
+		   
+			// Remove all embeds rewrite rules.
+			add_filter( 'rewrite_rules_array', function ($rules) {
+				foreach($rules as $rule => $rewrite) {
+					if(false !== strpos($rewrite, 'embed=true')) {
+						unset($rules[$rule]);
+					}
+				}
+				return $rules;
+			} );
+		   
+			// Remove filter of the oEmbed result before any HTTP requests are made.
+			remove_filter( 'pre_oembed_result', 'wp_filter_pre_oembed_result', 10 );
+		}, 9999 ); 
 
 		return $this;
 	}
